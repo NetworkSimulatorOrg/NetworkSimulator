80a420c81660d6eed3f44f4e3c29fa23d0d42558
William Eiffert <weiffert@gmail.com>
2020-04-04 12:56:51 -0500

    Update Protocol Interface

--- src/Protocol.java
+++ src/Protocol.java
@@ -1,6 +1,5 @@
 public interface Protocol {
     int sendMsg(Message msg);
+    int recvMsg(Message msg);
     int handleCollision();
-    int sendManagementMsg(Object msg);
-    Object recvManagementMsg();
 }
