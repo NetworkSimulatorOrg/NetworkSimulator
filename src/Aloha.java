2d30e97f4235d66256011411b220c1d7acb85a5d
William Eiffert <weiffert@gmail.com>
2020-04-07 21:00:28 -0500

    Comment specific uses of functions in Protocol

--- src/Aloha.java
+++ src/Aloha.java
@@ -1,8 +1,10 @@
 public class Aloha implements Protocol {
     @Override
     public ProtocolState sendMsg(Node node, Message msg) {
+        // Handle node state
         node.setSending(true);
 
+        // Send to all nodes
         for(Node adjacent : node.adjacent) {
             node.sendMsg(msg, adjacent.getId());
         }
@@ -10,14 +12,17 @@ public class Aloha implements Protocol {
         // Sleep the sending thread so that it doesn't try to send another until the first one would be received.
         node.sendingDelay();
 
+        // Handle node state
         node.setSending(false);
 
+        // No check for state of line, so always return success.
         return ProtocolState.Success;
     }
 
     @Override
     public ProtocolState recvMsg(Node node, Message msg) {
-        if(msg.isCorrupt() || (node.isSending() && node.isReceiving()))
+        // Check for corruption and collision
+        if(msg.isCorrupt())
             return ProtocolState.Failure;
         return ProtocolState.Success;
     }
--- src/Protocol.java
+++ src/Protocol.java
@@ -1,7 +1,21 @@
 import java.util.List;
 
 public interface Protocol {
+    /*
+     * Takes in the sending node, msg to send
+     * Handles node state sending changes.
+     *   MUST CALL NODE.SETSENDING() AND SENDINGDELAY();
+     * Handles collisions if protocol specifies.
+     * Returns when done sending
+     */
     ProtocolState sendMsg(Node node, Message msg);
+    /*
+     * Takes in sending node, the message received
+     * The propagation delay has already occurred to receive the message,
+     *   so no need to check for the node's current state.
+     * Returns SUCCESS if successful reception.
+     * Returns FAILURE if failed reception.
+     * Returns ERROR if an error occured.
+     */
     ProtocolState recvMsg(Node node, Message msg);
-    // int handleCollision(); This is done in the recvMsg and send msg.
 }
