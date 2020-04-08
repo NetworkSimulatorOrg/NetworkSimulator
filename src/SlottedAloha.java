a13798911306eda90a4153875ac9ab403a42aaa4
William Eiffert <weiffert@gmail.com>
2020-04-08 07:48:02 -0500

    Provide basic synchronize functionality with object notifyAll and wait

--- src/SlottedAloha.java
+++ src/SlottedAloha.java
@@ -3,16 +3,39 @@ import java.util.List;
 public class SlottedAloha extends Aloha implements Protocol{
 
     private int frameSize;
+    private Object sync;
+    private Thread synchroning;
 
     public SlottedAloha(int frameSize){
         this.frameSize = frameSize;
+        sync = new Object();
+
+        synchroning = new Thread(this::synchronizeThread);
+        synchroning.start();
+    }
+
+    private void synchronizeThread() {
+        // After waiting
+        while(true) {
+            sync.notifyAll();
+
+            try {
+                Thread.sleep(frameSize * 100);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
     }
 
     // @TODO
     public ProtocolState sendMsg(Node node, Message msg){
-        // If synchronized,
-        return super.sendMsg(node, msg);
-        // TODO: otherwise, exit and wait for next iteration?
+        try {
+            sync.wait();
+            return super.sendMsg(node, msg);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+        return ProtocolState.Error;
     }
 
     // @TODO
