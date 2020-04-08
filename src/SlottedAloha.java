f0754924f0aae036f00f2d65ce89da4101dfbca2
William Eiffert <weiffert@gmail.com>
2020-04-07 20:44:25 -0500

    Update Slotted Aloha to build off of Aloha

--- src/SlottedAloha.java
+++ src/SlottedAloha.java
@@ -1,24 +1,22 @@
 import java.util.List;
 
-public class SlottedAloha implements Protocol{
+public class SlottedAloha extends Aloha implements Protocol{
 
     private int frameSize;
 
     public SlottedAloha(int frameSize){
         this.frameSize = frameSize;
     }
-    
-    // @TODO
-    public ProtocolState sendMsg(Message msg, List<Node> id){
-        for(Node node : id){
-            node.sendMsg(msg, node.id);
-        }
 
-        return ProtocolState.Success;
+    // @TODO
+    public ProtocolState sendMsg(Node node, Message msg){
+        // If synchronized,
+        return super.sendMsg(node, msg);
+        // TODO: otherwise, exit and wait for next iteration?
     }
 
     // @TODO
-    public ProtocolState recvMsg(Message msg){
-        return ProtocolState.Fail;
+    public ProtocolState recvMsg(Node node, Message msg){
+        return super.recvMsg(node, msg);
     }
 }
