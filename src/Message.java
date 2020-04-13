fec64cd2646ba95c090b2396b21245b83e65baaf
William Eiffert <weiffert@gmail.com>
2020-04-13 08:40:31 -0500

    Create toString method for printing messages

--- src/Message.java
+++ src/Message.java
@@ -22,6 +22,26 @@ public class Message {
         return copy;
     }
 
+    @Override
+    public String toString() {
+        return toString("");
+    }
+
+    public String toString(String tab) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(
+                tab + "+-------------------------------+\n" +
+                tab + "| Originator: " + sender + "\t\t\t\t\t|\n" +
+                tab + "| Sequence number: " + sequenceNumber + "\t\t\t|\n" +
+                tab + "| Payload: " + payload + "\t|\n" +
+                tab + "+ - - Current Hop - - - - - - - +\n" +
+                tab + "| Last Sender: " + lastSender + "\t\t\t\t|\n" +
+                tab + "| Corrupt: " + corrupt + "\t\t\t\t|\n" +
+                tab + "+-------------------------------+\n"
+        );
+        return builder.toString();
+    }
+
     public String getSender() {
         return sender;
     }
