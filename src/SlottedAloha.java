fc4f6fb2ca73e2ca826db5b716e36f2adf1b2d31
Karl Gaertner <33812693+kgaertne@users.noreply.github.com>
2020-04-15 03:00:34 -0400

    Cleaned up Variables (Continued)

--- src/SlottedAloha.java
+++ src/SlottedAloha.java
@@ -1,12 +1,11 @@
 public class SlottedAloha extends Aloha implements Protocol{
 
-    private int frameSize;
     private Object sync;
     private Thread synchronizing;
     private volatile boolean synchronizingRunning;
 
-    public SlottedAloha(int frameSize){
-        this.frameSize = frameSize;
+    public SlottedAloha(){
+        // Frame size = longest path + 1 to allow for delay from traffic
         sync = new Object();
 
         synchronizing = new Thread(this::synchronizeThread);
@@ -19,7 +18,7 @@ public class SlottedAloha extends Aloha implements Protocol{
             sync.notifyAll();
 
             try {
-                Thread.sleep(frameSize * 100);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 synchronizingRunning = false;
             }
@@ -28,8 +27,38 @@ public class SlottedAloha extends Aloha implements Protocol{
 
     // @TODO
     public ProtocolState sendMsg(Node node, Message msg) throws InterruptedException {
-        sync.wait();
-        return super.sendMsg(node, msg);
+
+        // Keep resending the message until there is no collision
+        while (true){
+
+            // Wait until the next frame starts
+            sync.wait();
+
+            Protocol.sendMsgHelper(node, msg);
+
+            // Check if this message collided
+            if (msg.isCorrupt()){
+                // Report this as a collision
+                Protocol.sendReport(ReportType.Collision, msg, node.getId());
+                
+                // Resend the message at some future time.
+                int delay = (int) (Math.random() * Network.computeNodeCount * node.longestDistance * node.propagationRate);
+                System.out.println("Node " + node.getId() + " will be delayed for " + delay + " ms.");
+                Thread.sleep(delay);
+                msg.prepareForRetransmission();
+            }
+            else{
+                // Send a report that the message was successfully received by all nodes.
+                Protocol.sendReport(ReportType.Successful, msg, node.getId());
+                break;
+            }
+
+        }
+
+        // No check for state of line, so always return success.
+        return ProtocolState.Success;
+
+        //return super.sendMsg(node, msg);
     }
 
     // @TODO
